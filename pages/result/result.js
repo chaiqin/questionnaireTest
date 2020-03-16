4cee13abac881931691024535055364a32ed2358
chq <17875511931@163.com>
2020-03-16 19:53:05 +0800

    two

--- pages/result/result.js
+++ pages/result/result.js
@@ -6,18 +6,12 @@ let Charts = require('./../../utils/wxcharts.js');
 Page({
   data: {
     charcter:['完美型', '给予型', '实干型', '浪漫型', '观察型', '怀疑型','享乐型','统治型','调停型'],
+    numArr:[14,12,12,12,13,11,9,12,13], //题目各自数量
     info:[]
   },
   onLoad: function (options) {
     that = this;
     var data = JSON.parse(options.data)
-    // console.log(data)
-    // var data = [
-    //   //select:0未选择|1待选择|2已选择
-    //   { title: "你很容易迷惑。", characterType: 9, select: 0, conform: "try", inconform: "" },
-    //   { title: "你不想成为一个喜欢批评别人的人，但很难做到。", characterType: 1, select: 0, conform: "try", inconform: "" },
-    //   { title: "你喜欢研究宇宙的道理、哲理。", characterType: 5, select: 0, conform: "", inconform: "",isLast:1},
-    // ];
     //计算对应类型个数和总数
     var result=[0,0,0,0,0,0,0,0,0];
     var max=0;
@@ -27,6 +21,13 @@ Page({
         max++;
       }
     }
+    //计算性格占比
+    var perArr = [];
+    for(var j = 0;j<result.length;j++){
+      var per = parseInt(result[j]/this.data.numArr[j]*100);
+      perArr.push(per);
+    }
+
     new Charts({
       animation: true,
       canvasId: 'canvas',
@@ -34,13 +35,13 @@ Page({
       categories: this.data.charcter,
       series: [{
         name: '性格占比图',
-        data: result
+        data: perArr
       }],
       width: 360,
       height: 200,
       extra: {
         radar: {
-          max: max,//雷达数值的最大值
+          max: 100,//雷达数值的最大值
           // labelColor:"#2C2C2C",
         }
       }
@@ -50,15 +51,15 @@ Page({
     var img = "../../resources/img/character-";
     var arr = [];
     var oneObj = {},twoObj = {};
-    var temp = result.slice();
+    var temp = perArr.slice();
     var one = this.getMax(temp);
-    oneObj.per = temp[one]/max*100;
+    oneObj.per = temp[one];
     oneObj.charcter = this.data.charcter[one];
     oneObj.img = img+(one+1)+".png";
     arr.push(oneObj);
     temp[one]=0;
     var two = this.getMax(temp);
-    twoObj.per = temp[two]/max*100;
+    twoObj.per = temp[two];
     twoObj.charcter = this.data.charcter[two];
     twoObj.img = img+(two+1)+".png";
     arr.push(twoObj);
